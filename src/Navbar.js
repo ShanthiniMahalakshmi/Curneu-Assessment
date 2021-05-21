f20f9a02fb84c2a8ed0d02033bfc22d5db7288de
Shanthini Mahalakshmi <shanthinimahalakshmi18@gmail.com>
2021-05-21 20:11:45 +0530

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-
+//commit
 function Navbar() {
   const [click, setClick] = useState(false);
 
